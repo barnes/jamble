509c3fa4508b541caf2259568249cac13217d25a
Ryan Barnes <rybarnes@gmail.com>
2024-02-16 10:48:12 -0500

    stlying improvements, bug fixes

---
+++ src/lib/words.ts
@@ -0,0 +1,17 @@
+export const words = {
+  "3_letters": ["cat", "dog", "cow", "car", "van", "bus", "ink", "red", "blue", "green"],
+
+  "4_letters": ["wolf", "bear", "lion", "goat", "bull", "deer", "duck", "goose", "swan", "dove"],
+  
+  "5_letters": ["horse", "camel", "sheep", "goose", "swan", "mongoose", "finch", "juice", "bread", "pizza"],
+ 
+  "6_letters": ["donkey", "monkey", "rabbit", "weasel", "wallaby", "lizard", "falcon", "beetle", "spider", "insect"],
+  
+  "7_letters": ["buffalo", "dolphin", "porcupine", "dromedary", "turkey", "chicken", "parakeet", "goldfish", "tortoise", "hamster"],
+  
+  "8_letters": ["elephant", "gazelle", "squirrel", "reindeer", "cardinal", "raccoon", "chinchilla", "firefly", "caterpillar", "butterfly"],
+  
+  "9_letters": ["rhinoceros", "gorilla", "orangutan", "opossum", "salamander", "rattlesnake", "tarantula", "owl", "strawberry", "blackberry"],
+  
+  "10_letters": ["hippopotamus", "chimpanzee", "crocodile", "kangaroo", "cockroach", "koala", "blueberry", "watermelon", "dragonfruit", "pineapple"]  
+} 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,78 +1,119 @@
 <script lang="ts">
 	import type { W } from "vitest/dist/reporters-1evA5lom.js";
-
-	const words = {
-  "3_letters": ["cat", "dog", "cow", "pig", "fox"],
-  "4_letters": ["wolf", "bear", "lion", "goat", "deer"], 
-  "5_letters": ["horse", "zebra", "camel", "llama", "sheep"],
-  "6_letters": ["donkey", "monkey", "lemur", "bunny", "ferret"],
-  "7_letters": ["buffalo", "elephant", "dolphin", "raccoon", "squirrel"]
-};
+  import { words } from '$lib/words'
 
 	const getRandom = (set: number) => {
-		return words[Object.keys(words)[set]][Math.floor(Math.random() * 5)];
+    const keys = Object.keys(words);
+
+		return words[keys[set]][Math.floor(Math.random() * words[keys[set]].length)];
 	};
 
 	const shuffleWord = (word: string) => {
-		return word
+		const shuffled = word
 			.split('')
 			.sort(() => Math.random() - 0.5)
 			.join('');
+    if(shuffled === word ) {
+      return shuffleWord(word)
+    } else{
+      return shuffled
+    };
 	};
 
-	const todaysWords = [getRandom(0), getRandom(1), getRandom(2), getRandom(3)];
-	const shuffledWords = todaysWords.map((word) => shuffleWord(word));
-	console.log(todaysWords);
-	console.log(shuffledWords);
+  const getWords = () => {
+    const todaysWords = [getRandom(0), getRandom(1), getRandom(2), getRandom(3), getRandom(4), getRandom(5), getRandom(6), getRandom(7)];
+    const shuffledWords = todaysWords.map((word) => shuffleWord(word));
+    return {todaysWords, shuffledWords}
+  
+  }
+
+  let todaysWords = getWords();
+
+  console.log(todaysWords);
+
+	// const todaysWords = [getRandom(0), getRandom(1), getRandom(2), getRandom(3)];
+	// const shuffledWords = todaysWords.map((word) => shuffleWord(word));
 
 	let gameState = $state({
 		timer: 0,
-		state: 'start', //start, playing, win, loss
-		currentShuffle: shuffledWords[0],
-    currentWord: todaysWords[0],
-		guess: ''
+		state: 'start', //start, playing, end 
+		currentShuffle: todaysWords['shuffledWords'][0],
+    currentWord: todaysWords['todaysWords'][0],
+		guess: '',
+    correctCount: 0,
+    correctWords: [],
+    completeGame: false
 	});
+
+  console.log(gameState);
 
 	const startGame = () => {
 		gameState.state = 'playing';
 		gameState.timer = 30;
+     gameState.correctCount = 0;
+    gameState.correctWords = [];
 		const timer = setInterval(() => {
 			if (gameState.currentWord === gameState.guess) {
-				gameState.currentShuffle = shuffledWords[shuffledWords.indexOf(gameState.currentShuffle) + 1];
-        gameState.currentWord = todaysWords[todaysWords.indexOf(gameState.currentWord) + 1];
-        console.log(gameState.currentWord);
-        console.log(gameState.currentShuffle);
-
+        gameState.correctCount++;
+        gameState.correctWords.push(gameState.currentWord);
+				gameState.currentShuffle = todaysWords['shuffledWords'][todaysWords['shuffledWords'].indexOf(gameState.currentShuffle) + 1];
+        gameState.currentWord = todaysWords['todaysWords'][todaysWords['todaysWords'].indexOf(gameState.currentWord) + 1];
 				gameState.guess = '';
-				if (gameState.currentWord === undefined) {
-					gameState.state = 'win';
-				}
+        if(gameState.currentShuffle === undefined) {
+          clearInterval(timer);
+          gameState.completeGame = true;
+          gameState.state = 'end';
+        }
 			}
 			gameState.timer--;
 			if (gameState.timer <= 0) {
 				clearInterval(timer);
-				gameState.state = 'loss';
+				gameState.state = 'end';
 			}
 		}, 1000);
 	};
 
-	console.log(gameState);
 </script>
 
 <h1>Jamble</h1>
 <p>
-	You've got 30 seconds to un-jumble as many words as possible. Guess all 5 of todays words to win.
-	Come back tomorrow for a fresh puzzle!
+	You've got 30 seconds to un-jumble as many words as possible. Your score is the number of words you clear. 
 </p>
 
 {#if gameState.state == 'start'}
-	<button on:click={startGame}>Play!</button>
+ <div class="button">
+   <button on:click={startGame}>Play!</button>
+ </div>
 {:else if gameState.state == 'playing'}
 	{gameState.timer}
-	<h2>{gameState.currentShuffle}</h2>
+  <div class="word">{gameState.currentShuffle}</div>
 	<input id="gameInput" type="text" bind:value={gameState.guess} autofocus />
-{:else if gameState.state == 'win'}
-	<h2>You win!</h2>
-{:else if gameState.state == 'loss'}
-	<h2>You lose!</h2>
+{:else if gameState.state == 'end'}
+	<h2>Game over!</h2>
+  <ul>
+    <li>You got {gameState.correctCount} words correct!</li>
+    <li>Correct words: {gameState.correctWords.join(', ')}</li>
+    {#if gameState.correctCount === 8}
+      <li>Well done! You got all the words!</li>
+    {:else}
+      <li>The last word you did not get was {gameState.currentWord}</li>
+    {/if}
+  </ul>
+  <div class="button">
+    <button on:click={startGame}>Play Again!</button>
+  </div>
 {/if}
+
+<style>
+  .word {
+    font-size: 4rem;
+    margin: 1rem 0;
+    display:flex;
+    justify-content: center;
+  }
+  .button {
+    margin: 1rem 0;
+    display: flex;
+    justify-content: center;
+  }
+</style>
